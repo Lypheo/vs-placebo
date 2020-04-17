d307259363acd957872315618302771e25cbb625
Frechdachs <frechdachs@rekt.cc>
2020-04-17 02:30:34 +0200

    Change sx/sy to be consistend with other resizers and rename param0/1 to param1/2 to be consistent with the mpv docs
    This also fixes a chroma shift bug.

--- resample.h
+++ resample.h
@@ -1,12 +1,8 @@
-//
-// Created by saifu on 4/14/2020.
-//
-
-#ifndef P2P_RESAMPLE_H
-#define P2P_RESAMPLE_H
+#ifndef VS_PLACEBO_RESAMPLE_H
+#define VS_PLACEBO_RESAMPLE_H
 
 #include "VapourSynth.h"
 
 void VS_CC ResampleCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi);
 
-#endif //P2P_RESAMPLE_H
+#endif //VS_PLACEBO_RESAMPLE_H
