568eb5de31b05edc39a76adde55fa769e8ee3fe2
quietvoid <39477805+quietvoid@users.noreply.github.com>
2023-01-18 19:54:03 -0500

    Support libdovi 3.0.0

--- src/dovi_meta.h
+++ src/dovi_meta.h
@@ -13,13 +13,57 @@
             if (hdr->use_prev_vdr_rpu_flag)
                 goto done;
 
-            const DoviRpuDataMapping *vdm = dovi_rpu_get_data_mapping(rpu);
-            if (!vdm)
-                goto skip_vdm;
+            const DoviRpuDataMapping *mapping = dovi_rpu_get_data_mapping(rpu);
+            if (!mapping)
+                goto skip_mapping;
 
             const uint8_t bits = hdr->bl_bit_depth_minus8 + 8;
             const float scale = 1.0f / (1 << hdr->coefficient_log2_denom);
 
+        #if RPU_PARSER_MAJOR >= 3
+            for (int c = 0; c < 3; c++) {
+                const DoviReshapingCurve curve = mapping->curves[c];
+
+                struct pl_reshape_data *cmp = &dovi_meta.comp[c];
+                cmp->num_pivots = curve.pivots.len;
+                memset(cmp->method, curve.mapping_idc, sizeof(cmp->method));
+
+                uint16_t pivot = 0;
+                for (int pivot_idx = 0; pivot_idx < cmp->num_pivots; pivot_idx++) {
+                    pivot += curve.pivots.data[pivot_idx];
+                    cmp->pivots[pivot_idx] = (float) pivot / ((1 << bits) - 1);
+                }
+
+                for (int i = 0; i < cmp->num_pivots - 1; i++) {
+                    memset(cmp->poly_coeffs[i], 0, sizeof(cmp->poly_coeffs[i]));
+
+                    if (curve.polynomial) {
+                        const DoviPolynomialCurve *poly_curve = curve.polynomial;
+
+                        for (int k = 0; k <= poly_curve->poly_order_minus1.data[i] + 1; k++) {
+                            int64_t ipart = poly_curve->poly_coef_int.list[i]->data[k];
+                            uint64_t fpart = poly_curve->poly_coef.list[i]->data[k];
+                            cmp->poly_coeffs[i][k] = ipart + scale * fpart;
+                        }
+                    } else if (curve.mmr) {
+                        const DoviMMRCurve *mmr_curve = curve.mmr;
+
+                        int64_t ipart = mmr_curve->mmr_constant_int.data[i];
+                        uint64_t fpart = mmr_curve->mmr_constant.data[i];
+                        cmp->mmr_constant[i] = ipart + scale * fpart;
+                        cmp->mmr_order[i] = mmr_curve->mmr_order_minus1.data[i] + 1;
+
+                        for (int j = 1; j <= cmp->mmr_order[i]; j++) {
+                            for (int k = 0; k < 7; k++) {
+                                ipart = mmr_curve->mmr_coef_int.list[i]->list[j]->data[k];
+                                fpart = mmr_curve->mmr_coef.list[i]->list[j]->data[k];
+                                cmp->mmr_coeffs[i][j - 1][k] = ipart + scale * fpart;
+                            }
+                        }
+                    }
+                }
+            }
+        #else
             for (int c = 0; c < 3; c++) {
                 struct pl_reshape_data *cmp = &dovi_meta.comp[c];
                 uint16_t pivot = 0;
@@ -31,25 +75,25 @@
 
                 for (int i = 0; i < cmp->num_pivots - 1; i++) {
                     memset(cmp->poly_coeffs[i], 0, sizeof(cmp->poly_coeffs[i]));
-                    cmp->method[i] = vdm->mapping_idc[c].data[i];
+                    cmp->method[i] = mapping->mapping_idc[c].data[i];
 
                     switch (cmp->method[i]) {
                     case 0: // polynomial
-                        for (int k = 0; k <= vdm->poly_order_minus1[c].data[i] + 1; k++) {
-                            int64_t ipart = vdm->poly_coef_int[c].list[i]->data[k];
-                            uint64_t fpart = vdm->poly_coef[c].list[i]->data[k];
+                        for (int k = 0; k <= mapping->poly_order_minus1[c].data[i] + 1; k++) {
+                            int64_t ipart = mapping->poly_coef_int[c].list[i]->data[k];
+                            uint64_t fpart = mapping->poly_coef[c].list[i]->data[k];
                             cmp->poly_coeffs[i][k] = ipart + scale * fpart;
                         }
                         break;
                     case 1: // MMR
-                        int64_t ipart = vdm->mmr_constant_int[c].data[i];
-                        uint64_t fpart = vdm->mmr_constant[c].data[i];
+                        int64_t ipart = mapping->mmr_constant_int[c].data[i];
+                        uint64_t fpart = mapping->mmr_constant[c].data[i];
                         cmp->mmr_constant[i] = ipart + scale * fpart;
-                        cmp->mmr_order[i] = vdm->mmr_order_minus1[c].data[i] + 1;
+                        cmp->mmr_order[i] = mapping->mmr_order_minus1[c].data[i] + 1;
                         for (int j = 1; j <= cmp->mmr_order[i]; j++) {
                             for (int k = 0; k < 7; k++) {
-                                ipart = vdm->mmr_coef_int[c].list[i]->list[j]->data[k];
-                                fpart = vdm->mmr_coef[c].list[i]->list[j]->data[k];
+                                ipart = mapping->mmr_coef_int[c].list[i]->list[j]->data[k];
+                                fpart = mapping->mmr_coef[c].list[i]->list[j]->data[k];
                                 cmp->mmr_coeffs[i][j - 1][k] = ipart + scale * fpart;
                             }
                         }
@@ -57,9 +101,10 @@
                     }
                 }
             }
+        #endif
 
-            dovi_rpu_free_data_mapping(vdm);
-        skip_vdm:
+            dovi_rpu_free_data_mapping(mapping);
+        skip_mapping:
 
             if (hdr->vdr_dm_metadata_present_flag) {
                 const DoviVdrDmData *dm_data = dovi_rpu_get_vdr_dm_data(rpu);
